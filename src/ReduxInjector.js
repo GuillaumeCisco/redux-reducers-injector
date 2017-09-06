99fa524fb1d91210316e4339196f591ab329b748
GuillaumeCisco <guillaumecisco@gmail.com>
2017-09-06 12:01:39 +0200

    add codebase

---
+++ src/ReduxInjector.js
@@ -0,0 +1,59 @@
+import {createStore, combineReducers} from 'redux';
+import {set, has} from 'lodash';
+
+let store = {};
+let combine = combineReducers;
+
+export function combineReducersRecurse(reducers) {
+    // If this is a leaf or already combined.
+    if (typeof reducers === 'function') {
+        return reducers;
+    }
+
+    // If this is an object of functions, combine reducers.
+    if (typeof reducers === 'object') {
+        let combinedReducers = {};
+        for (let key of Object.keys(reducers)) {
+            combinedReducers[key] = combineReducersRecurse(reducers[key]);
+        }
+        return combine(combinedReducers);
+    }
+
+    // If we get here we have an invalid item in the reducer path.
+    throw new Error({
+        message: 'Invalid item in reducer tree',
+        item: reducers
+    });
+}
+
+export function createInjectStore(initialReducers, ...args) {
+    // If last item is an object, it is overrides.
+    if (typeof args[args.length - 1] === 'object') {
+        const overrides = args.pop();
+        // Allow overriding the combineReducers function such as with redux-immutable.
+        if (overrides.hasOwnProperty('combineReducers') && typeof overrides.combineReducers === 'function') {
+            combine = overrides.combineReducers;
+        }
+    }
+
+    store = createStore(
+        combineReducersRecurse(initialReducers),
+        ...args
+    );
+
+    store.injectedReducers = initialReducers;
+
+    return store;
+}
+
+export function reloadReducer(key, reducer) {
+    store.replaceReducer(combineReducersRecurse({...store.injectedReducers, [key]: reducer}));
+}
+
+export function injectReducer(key, reducer, force = false) {
+    // If already set, do nothing.
+    if (!has(store.injectedReducers, key) || force) {
+        set(store.injectedReducers, key, reducer);
+        store.replaceReducer(combineReducersRecurse(store.injectedReducers));
+    }
+}
